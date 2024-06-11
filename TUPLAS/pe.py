726816b0964713045f057738456bc1fef67b74b3
Samuel Imanol <167726711+Samueljustiniani@users.noreply.github.com>
2024-06-11 09:03:04 -0500

    Add files via upload

---
+++ TUPLAS/pe.py
@@ -0,0 +1,10 @@
+#Calcular el promedio de notas de estudiantes usando tuplas
+
+notas_estudiante1 = (85, 90, 88, 92)
+
+total = sum(notas_estudiante1)
+promedio_estudiante1 = total / len(notas_estudiante1)
+
+print("Promedio del estudiante 1:", promedio_estudiante1)
+
+
